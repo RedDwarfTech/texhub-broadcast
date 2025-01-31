49a5106dc0529448a0082d79fb13d8dc749d1d7c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-01-31 21:01:54 +0800

    feat: add socket io provider

---
+++ src/websocket/conn/socket_io_provider.ts
@@ -0,0 +1,320 @@
+// @ts-ignore
+import { Observable } from "lib0/observable";
+// @ts-ignore
+import Y from "yjs";
+// @ts-ignore
+import * as awarenessProtocol from "y-protocols/awareness";
+import {
+  createEncoder,
+  toUint8Array,
+  writeVarUint,
+  writeVarUint8Array,
+  readVarUint8Array,
+  createDecoder,
+  readVarUint,
+  // @ts-ignore
+} from "lib0/dist/encoding.cjs";
+// @ts-ignore
+import * as syncProtocol from "y-protocols/sync";
+// @ts-ignore
+import * as authProtocol from "y-protocols/auth";
+// @ts-ignore
+import * as url from "lib0/url";
+// @ts-ignore
+import * as encoding from "lib0/encoding";
+// @ts-ignore
+import * as bc from "lib0/broadcastchannel";
+// @ts-ignore
+import * as time from 'lib0/time';
+
+export const messageSync = 0;
+export const messageQueryAwareness = 3;
+export const messageAwareness = 1;
+export const messageAuth = 2;
+
+const messageHandlers: any[] = [];
+
+messageHandlers[messageSync] = (
+  encoder: any,
+  decoder: any,
+  provider: any,
+  emitSynced: any,
+  _messageType: any
+) => {
+  writeVarUint(encoder, messageSync);
+  const syncMessageType = syncProtocol.readSyncMessage(
+    decoder,
+    encoder,
+    provider.doc,
+    provider
+  );
+  if (
+    emitSynced &&
+    syncMessageType === syncProtocol.messageYjsSyncStep2 &&
+    !provider.synced
+  ) {
+    provider.synced = true;
+  }
+};
+
+messageHandlers[messageQueryAwareness] = (
+  encoder: any,
+  _decoder: any,
+  provider: any,
+  _emitSynced: any,
+  _messageType: any
+) => {
+  writeVarUint(encoder, messageAwareness);
+  writeVarUint8Array(
+    encoder,
+    awarenessProtocol.encodeAwarenessUpdate(
+      provider.awareness,
+      Array.from(provider.awareness.getStates().keys())
+    )
+  );
+};
+
+messageHandlers[messageAwareness] = (
+  _encoder: any,
+  decoder: any,
+  provider: any,
+  _emitSynced: any,
+  _messageType: any
+) => {
+  awarenessProtocol.applyAwarenessUpdate(
+    provider.awareness,
+    readVarUint8Array(decoder),
+    provider
+  );
+};
+
+messageHandlers[messageAuth] = (
+  _encoder: any,
+  decoder: any,
+  provider: any,
+  _emitSynced: any,
+  _messageType: any
+) => {
+  authProtocol.readAuthMessage(decoder, provider.doc, (_ydoc, reason) =>
+    permissionDeniedHandler(provider, reason)
+  );
+};
+
+// @todo - this should depend on awareness.outdatedTime
+const messageReconnectTimeout = 30000;
+
+/**
+ * @param {WebsocketProvider} provider
+ * @param {string} reason
+ */
+const permissionDeniedHandler = (provider: any, reason: any) =>
+  console.warn(`Permission denied to access ${provider.url}.\n${reason}`);
+
+/**
+ * @param {WebsocketProvider} provider
+ * @param {Uint8Array} buf
+ * @param {boolean} emitSynced
+ * @return {encoding.Encoder}
+ */
+const readMessage = (provider: any, buf: any, emitSynced: any) => {
+  const decoder = createDecoder(buf);
+  const encoder = createEncoder();
+  const messageType = readVarUint(decoder);
+  const messageHandler = provider.messageHandlers[messageType];
+  if (/** @type {any} */ messageHandler) {
+    messageHandler(encoder, decoder, provider, emitSynced, messageType);
+  } else {
+    console.error("Unable to compute message");
+  }
+  return encoder;
+};
+
+/**
+ * @param {WebsocketProvider} provider
+ * @param {ArrayBuffer} buf
+ */
+const broadcastMessage = (provider: any, buf: any) => {
+  const ws = provider.ws;
+  if (provider.wsconnected && ws && ws.readyState === ws.OPEN) {
+    ws.send(buf);
+  }
+  if (provider.bcconnected) {
+    bc.publish(provider.bcChannel, buf, provider);
+  }
+};
+
+export class SocketIOProvider extends Observable<string> {
+  maxBackoffTime: number;
+  bcChannel: string;
+  url: string;
+  roomname: string;
+  doc: Y.Doc;
+  _WS: {
+    new (url: string | URL, protocols?: string | string[]): WebSocket;
+    prototype: WebSocket;
+    readonly CONNECTING: 0;
+    readonly OPEN: 1;
+    readonly CLOSING: 2;
+    readonly CLOSED: 3;
+  };
+  awareness: awarenessProtocol.Awareness;
+  wsconnected: boolean;
+  wsconnecting: boolean;
+  bcconnected: boolean;
+  disableBc: boolean;
+  wsUnsuccessfulReconnects: number;
+  messageHandlers: any;
+  _synced: boolean;
+  ws: any;
+  wsLastMessageReceived: number;
+  shouldConnect: boolean;
+  _resyncInterval: any;
+  _bcSubscriber: (data: any, origin: any) => void;
+  _updateHandler: (update: any, origin: any) => void;
+  _awarenessUpdateHandler: (
+    { added, updated, removed }: { added: any; updated: any; removed: any },
+    _origin: any
+  ) => void;
+  _unloadHandler: () => void;
+  _checkInterval: NodeJS.Timeout;
+
+  constructor(
+    serverUrl: string,
+    roomname: string,
+    doc: Y.Doc,
+    {
+      connect = true,
+      awareness = new awarenessProtocol.Awareness(doc),
+      params = {},
+      WebSocketPolyfill = WebSocket,
+      resyncInterval = -1,
+      maxBackoffTime = 2500,
+      disableBc = false,
+    } = {}
+  ) {
+    super();
+    // ensure that url is always ends with /
+    while (serverUrl[serverUrl.length - 1] === "/") {
+      serverUrl = serverUrl.slice(0, serverUrl.length - 1);
+    }
+    const encodedParams = url.encodeQueryParams(params);
+    this.maxBackoffTime = maxBackoffTime;
+    this.bcChannel = serverUrl + "/" + roomname;
+    this.url =
+      serverUrl +
+      "/" +
+      roomname +
+      (encodedParams.length === 0 ? "" : "?" + encodedParams);
+    this.roomname = roomname;
+    this.doc = doc;
+    this._WS = WebSocketPolyfill;
+    this.awareness = awareness;
+    this.wsconnected = false;
+    this.wsconnecting = false;
+    this.bcconnected = false;
+    this.disableBc = disableBc;
+    this.wsUnsuccessfulReconnects = 0;
+    this.messageHandlers = messageHandlers.slice();
+    /**
+     * @type {boolean}
+     */
+    this._synced = false;
+    /**
+     * @type {WebSocket?}
+     */
+    this.ws = null;
+    this.wsLastMessageReceived = 0;
+    /**
+     * Whether to connect to other peers or not
+     * @type {boolean}
+     */
+    this.shouldConnect = connect;
+
+    /**
+     * @type {number}
+     */
+    this._resyncInterval = 0;
+    if (resyncInterval > 0) {
+      this._resyncInterval = /** @type {any} */ setInterval(() => {
+        if (this.ws && this.ws.readyState === WebSocket.OPEN) {
+          // resend sync step 1
+          const encoder = createEncoder();
+          writeVarUint(encoder, messageSync);
+          syncProtocol.writeSyncStep1(encoder, doc);
+          this.ws.send(toUint8Array(encoder));
+        }
+      }, resyncInterval);
+    }
+
+    /**
+     * @param {ArrayBuffer} data
+     * @param {any} origin
+     */
+    this._bcSubscriber = (data, origin) => {
+      if (origin !== this) {
+        const encoder = readMessage(this, new Uint8Array(data), false);
+        if (encoding.length(encoder) > 1) {
+          bc.publish(this.bcChannel, encoding.toUint8Array(encoder), this);
+        }
+      }
+    };
+    /**
+     * Listens to Yjs updates and sends them to remote peers (ws and broadcastchannel)
+     * @param {Uint8Array} update
+     * @param {any} origin
+     */
+    this._updateHandler = (update, origin) => {
+      if (origin !== this) {
+        const encoder = encoding.createEncoder();
+        encoding.writeVarUint(encoder, messageSync);
+        syncProtocol.writeUpdate(encoder, update);
+        broadcastMessage(this, encoding.toUint8Array(encoder));
+      }
+    };
+    this.doc.on("update", this._updateHandler);
+    /**
+     * @param {any} changed
+     * @param {any} _origin
+     */
+    this._awarenessUpdateHandler = ({ added, updated, removed }, _origin) => {
+      const changedClients = added.concat(updated).concat(removed);
+      const encoder = encoding.createEncoder();
+      encoding.writeVarUint(encoder, messageAwareness);
+      encoding.writeVarUint8Array(
+        encoder,
+        awarenessProtocol.encodeAwarenessUpdate(awareness, changedClients)
+      );
+      broadcastMessage(this, encoding.toUint8Array(encoder));
+    };
+    this._unloadHandler = () => {
+      awarenessProtocol.removeAwarenessStates(
+        this.awareness,
+        [doc.clientID],
+        "window unload"
+      );
+    };
+    if (typeof window !== "undefined") {
+      window.addEventListener("unload", this._unloadHandler);
+    } else if (typeof process !== "undefined") {
+      process.on("exit", this._unloadHandler);
+    }
+    awareness.on("update", this._awarenessUpdateHandler);
+    this._checkInterval = /** @type {any} */ setInterval(() => {
+      if (
+        this.wsconnected &&
+        messageReconnectTimeout <
+          time.getUnixTime() - this.wsLastMessageReceived
+      ) {
+        // no message received in a long time - not even your own awareness
+        // updates (which are updated every 15 seconds)
+        /** @type {WebSocket} */ this.ws.close();
+      }
+    }, messageReconnectTimeout / 10);
+    if (connect) {
+      this.connect();
+    }
+  }
+  connect() {
+    throw new Error("Method not implemented.");
+  }
+}
