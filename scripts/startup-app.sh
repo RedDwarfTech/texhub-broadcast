424ad38028f2eb67bf7f0f7ee5a1e6a1896a4419
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-02-03 16:33:52 +0800

    refactor: tweak the storage

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -2,7 +2,7 @@
 
 export HOST=0.0.0.0
 export PORT=1234
-export YPERSISTENCE=/opt/data/yjs-storage
+export YPERSISTENCE=/opt/data/yjs-storage-socketio
 # export NODE_OPTIONS="--max-old-space-size=256"
 
 # https://stackoverflow.com/questions/77357320/is-it-possible-to-change-the-node-heap-size
