706e5b16f4548d0a1bab8c3ba363f1740f1e7dec
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-02-10 21:10:34 +0800

    refactor: add error log

--- src/types/textypes.ts
+++ src/types/textypes.ts
@@ -3,7 +3,17 @@ import { ManagerOptions, Socket, SocketOptions, io } from "socket.io-client";
 export class MySocket {
   constructor(url: string, options?: Partial<ManagerOptions & SocketOptions>) {
     console.log("initial the websocket...." + url);
-    const doc: Socket = io(url, options);
-    return doc;
+    const socket: Socket = io(url, options);
+    socket.on("connect_error", (err:any) => {
+      // the reason of the error, for example "xhr poll error"
+      console.log(err.message);
+    
+      // some additional description, for example the status code of the initial HTTP response
+      console.log(err.description);
+    
+      // some additional context, for example the XMLHttpRequest object
+      console.log(err.context);
+    });
+    return socket;
   }
 }
