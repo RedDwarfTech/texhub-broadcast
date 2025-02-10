ce6954f9c830093f1dfd4420618059c555ccefac
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-02-10 19:49:58 +0800

    feat: add log

--- src/types/textypes.ts
+++ src/types/textypes.ts
@@ -2,6 +2,7 @@ import { ManagerOptions, Socket, SocketOptions, io } from "socket.io-client";
 
 export class MySocket {
   constructor(url: string, options?: Partial<ManagerOptions & SocketOptions>) {
+    console.log("initial the websocket...." + url);
     const doc: Socket = io(url, options);
     return doc;
   }
