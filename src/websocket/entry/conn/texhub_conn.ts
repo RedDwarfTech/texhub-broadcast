10c9db4bd3b983655b3e8c89e01336bbada79e1a
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-02-20 21:46:46 +0800

    feat: add path alias

--- src/websocket/entry/conn/texhub_conn.ts
+++ src/websocket/entry/conn/texhub_conn.ts
@@ -1,17 +1,11 @@
 import { Socket } from "socket.io";
 import { websocketServer } from "../../../app.js";
-import logger from "../../../common/log4js_config.js";
-import { setupWSConnection } from "../../../websocket/config/setup.js";
-import { toJSON } from "flatted";
+import { setupWSConnection } from "@websocket/config/setup.js";
 
 export const init_texconn = () => {
   let texhubNs = websocketServer.of("/texhub");
 
   texhubNs.on("connection", (socket: Socket) => {
-    if (logger.isDebugEnabled()) {
-      logger.debug("connection....");
-    }
-    logger.info("connection:" + toJSON(socket));
     setupWSConnection(socket, socket.request);
   });
 };
