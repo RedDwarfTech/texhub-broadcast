e5a252262914b553013deb8bf1a1fba998573cf1
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-02-26 22:08:15 +0800

    refactor: tweak the code

--- src/common/migration/leveldb_to_postgresql.ts
+++ src/common/migration/leveldb_to_postgresql.ts
@@ -1,24 +1,21 @@
-const persistenceDir = process.env.YPERSISTENCE;
 // @ts-ignore
-import { LeveldbPersistence } from "y-leveldb";
-const ldb = new LeveldbPersistence(persistenceDir);
+import { levelup } from "levelup";
+// @ts-ignore
+import leveldown from "leveldown";
+const persistenceDir = process.env.YPERSISTENCE;
+var db = levelup(leveldown(persistenceDir))
 
-// 遍历所有 key 的函数
 export async function iterateAllKeys(): Promise<void> {
-  // 创建一个 key 流
-  const keyStream = ldb.createKeyStream();
+  const keyStream = db.createKeyStream();
 
-  // 监听数据事件，获取每个 key
   keyStream.on('data', (key: string) => {
     console.log('Key:', key);
   });
 
-  // 监听结束事件
   keyStream.on('end', () => {
     console.log('All keys have been iterated.');
   });
 
-  // 监听错误事件
   keyStream.on('error', (err: Error) => {
     console.error('Error:', err);
   });
