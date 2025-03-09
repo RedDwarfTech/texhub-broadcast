a50a3cfef424e74d22d73a92b919b5fc994efc77
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-03-09 14:16:35 +0800

    refactor: tweak the log

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -4,6 +4,7 @@ import * as Y from "yjs";
 import { throttledFn } from "./appfile.js";
 import { Persistence } from "../model/yjs/Persistence.js";
 import { PostgresqlPersistance } from "./adapter/postgresql/postgresql_persistance.js";
+import logger from "../common/log4js_config.js";
 
 export let persistencePostgresql: Persistence;
 
@@ -13,16 +14,20 @@ if (typeof persistenceDir === "string") {
   persistencePostgresql = {
     provider: postgresqlDb,
     bindState: async (docName: string, ydoc: Y.Doc) => {
-      const persistedYdoc: Y.Doc = await postgresqlDb.getYDoc(docName);
-      const newUpdates: Uint8Array = Y.encodeStateAsUpdate(ydoc);
-      await postgresqlDb.storeUpdate(docName, newUpdates);
-      Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc));
-      ydoc.on("update", async (update: Uint8Array) => {
-        await postgresqlDb.storeUpdate(docName, update);
-        if (persistedYdoc) {
-          throttledFn(docName, postgresqlDb);
-        }
-      });
+      try {
+        const persistedYdoc: Y.Doc = await postgresqlDb.getYDoc(docName);
+        const newUpdates: Uint8Array = Y.encodeStateAsUpdate(ydoc);
+        await postgresqlDb.storeUpdate(docName, newUpdates);
+        Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc));
+        ydoc.on("update", async (update: Uint8Array) => {
+          await postgresqlDb.storeUpdate(docName, update);
+          if (persistedYdoc) {
+            throttledFn(docName, postgresqlDb);
+          }
+        });
+      } catch (err: any) {
+        logger.error("process update failed", err);
+      }
     },
     writeState: async (docName: string, ydoc: Y.Doc) => {},
   };
