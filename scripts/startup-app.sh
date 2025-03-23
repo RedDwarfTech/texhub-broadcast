31d5aefe2b3f5c9fa38b4e135035f8412f02f48e
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-03-23 19:18:17 +0800

    refactor: tweak the node debug

--- scripts/startup-app.sh
+++ scripts/startup-app.sh
@@ -2,10 +2,11 @@
 
 export HOST=0.0.0.0
 export PORT=1234
+export NODE_DEBUG=net
 export YPERSISTENCE=/opt/data/yjs-storage-socketio
 # export NODE_OPTIONS="--max-old-space-size=256"
 
 # https://stackoverflow.com/questions/77357320/is-it-possible-to-change-the-node-heap-size
-nohup NODE_DEBUG=net node --max-old-space-size=256 --max-semi-space-size=128 --inspect=0.0.0.0:9090 ./dist/app.js >> ws.log 2>&1 &
+nohup node --max-old-space-size=256 --max-semi-space-size=128 --inspect=0.0.0.0:9090 ./dist/app.js >> ws.log 2>&1 &
 
 tail -f ws.log
