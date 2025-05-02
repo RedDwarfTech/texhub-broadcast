2bda5175ebb48b0e4eae202f10342ffd82244770
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-05-02 15:19:31 +0800

    feat: add index style export

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { SocketIOClientProvider } from './websocket/conn/socket_io_client_provider.js';
+
+// 根据需要可以导出其他常用组件
+// export { SingleClientProvider } from './websocket/conn/single_client_provider.js';
+// export { SyncMessageType } from './model/texhub/sync_msg_type.js'; 
