b44d2b4400435767ee6e50c4df34a03d14791a10
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-05-04 01:07:33 +0800

    refactor: only initial log4js in node

--- src/common/log4js_config.ts
+++ src/common/log4js_config.ts
@@ -1,29 +1,51 @@
 import log4js, { Configuration } from "log4js";
-let logConfig: Configuration = {
-  appenders: {
-    out: {
-      type: "stdout",
-      layout: {
-        type: "colored",
+
+let logger: any;
+
+// Check if we're in a Node.js environment
+if (typeof window === 'undefined') {
+  // Node.js environment
+  const logConfig: Configuration = {
+    appenders: {
+      out: {
+        type: "stdout",
+        layout: {
+          type: "colored",
+        },
+      },
+      files: {
+        type: "file",
+        filename: "testing.log",
       },
     },
-    files: {
-      type: "file",
-      filename: "testing.log",
+    categories: {
+      default: {
+        appenders: ["out", "files"],
+        level: "all",
+        // https://stackoverflow.com/questions/79536525/is-it-possible-to-output-the-full-callstack-in-nodejs
+        enableCallStack: true,
+      },
     },
-  },
-  categories: {
-    default: {
-      appenders: ["out", "files"],
-      level: "all",
-      // https://stackoverflow.com/questions/79536525/is-it-possible-to-output-the-full-callstack-in-nodejs
-      enableCallStack: true,
-    },
-    
-  },
-  disableClustering: true,
-};
-log4js.configure(logConfig);
-let logger = log4js.getLogger();
-logger.level = "all";
+    disableClustering: true,
+  };
+  
+  log4js.configure(logConfig);
+  logger = log4js.getLogger();
+  logger.level = "all";
+} else {
+  // Browser environment
+  // Create a minimal logger implementation for browser compatibility
+  logger = {
+    trace: console.log,
+    debug: console.debug,
+    info: console.info,
+    warn: console.warn,
+    error: console.error,
+    fatal: console.error,
+    // Add a noop level property to prevent errors
+    set level(_) {},
+    get level() { return "all"; }
+  };
+}
+
 export default logger;
