92f29b9bc0f447259b7d03df3a0b310c9a8a4931
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-05-04 23:39:53 +0800

    feat: add socket io provider export

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@
 // Export SingleClientProvider for external projects
 import SingleClientProvider from './websocket/conn/single_client_provider';
 export { SingleClientProvider };
+
+import { SocketIOClientProvider } from './websocket/conn/socket_io_client_provider';
+export { SocketIOClientProvider };
