204dbebbae5728f7aaab26301f3abc9c7ad0e319
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-07-13 20:32:51 +0800

    refactor: sequelize for browser

--- src/storage/adapter/postgresql/conf/sequelize.ts
+++ src/storage/adapter/postgresql/conf/sequelize.ts
@@ -2,18 +2,24 @@ import { Sequelize } from 'sequelize';
 import { dbConfig } from './db_config.js';
 import logger from '@/common/log4js_config.js';
 
-export const sequelize = new Sequelize({
-  dialect: 'postgres',
-  host: dbConfig.host,
-  port: dbConfig.port,
-  username: dbConfig.user,
-  password: dbConfig.password,
-  database: dbConfig.database,
-  logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-}); 
+let sequelize: Sequelize | undefined = undefined;
+
+if (typeof window === 'undefined') {
+  sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: dbConfig.host,
+    port: dbConfig.port,
+    username: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database,
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  });
+}
+
+export { sequelize }; 
