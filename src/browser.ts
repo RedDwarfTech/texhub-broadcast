7b4487b5bc58b35d94a54e58e1e25d4316415646
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-07-14 22:10:57 +0800

    feat: add browser.js

---
+++ src/browser.ts
@@ -0,0 +1,9 @@
+// Main package entry point for external projects
+
+// Export SingleClientProvider for external projects
+
+import SingleClientProvider from './websocket/conn/single_client_provider';
+export { SingleClientProvider };
+
+import { SocketIOClientProvider } from './websocket/conn/socket_io_client_provider';
+export { SocketIOClientProvider };
