50545e6b34e58777c7105ff737becb0a6d37434d
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-08-02 10:03:54 +0800

    feat: tweak the offset handle

--- src/controllers/doc/doc_controller.ts
+++ src/controllers/doc/doc_controller.ts
@@ -1,10 +1,16 @@
 const persistenceDir = process.env.YPERSISTENCE;
 import express, { Request, Response, Router } from "express";
 import { initTpl } from "@collar/yjs_utils.js";
-import { calcFileVersion, calcProjectVersion, getProjectScrollVersion, getProjectVersionDetail } from "@/service/version_service.js";
+import {
+  calcFileVersion,
+  calcProjectVersion,
+  getProjectScrollVersion,
+  getProjectVersionDetail,
+} from "@/service/version_service.js";
 import logger from "@/common/log4js_config.js";
 import { AppResponse } from "@/texhub/biz/AppResponse.js";
 import { ProjectScrollVersionAttributes } from "@/model/texhub/project_scroll_version";
+import { MAX_I64 } from "@/common/app/global_constant.js";
 
 export const routerDoc: Router = express.Router();
 
@@ -19,22 +25,33 @@ routerDoc.get("/", async (req: Request, res: Response) => {
 
 routerDoc.get("/version/proj/scroll", async (req: Request, res: Response) => {
   const projId = req.query.projId;
-  const offset = req.query.offset;
-  const pageSize = req.query.pageSize;
-  let versions = await getProjectScrollVersion(projId!.toString(), offset!.toString(), parseInt(pageSize?.toString()!));
+  const offset = req.query.offset
+    ? req.query.offset?.toString()!
+    : MAX_I64.toString();
+  const pageSize = req.query.pageSize
+    ? parseInt(req.query.pageSize?.toString()!)
+    : 10;
+  let versions = await getProjectScrollVersion(
+    projId!.toString(),
+    offset,
+    pageSize
+  );
   res.send(versions);
 });
 
-routerDoc.get("/version/proj/scroll/detail", async (req: Request, res: Response) => {
-  const id = req.query.id;
-  let version = await getProjectVersionDetail(id!.toString());
-  let response: AppResponse<ProjectScrollVersionAttributes> = {
-    result: version,
-    message: "success",
-    code: 200,
-  };
-  res.send(response);
-});
+routerDoc.get(
+  "/version/proj/scroll/detail",
+  async (req: Request, res: Response) => {
+    const id = req.query.id;
+    let version = await getProjectVersionDetail(id!.toString());
+    let response: AppResponse<ProjectScrollVersionAttributes> = {
+      result: version,
+      message: "success",
+      code: 200,
+    };
+    res.send(response);
+  }
+);
 
 routerDoc.get("/version/file/scroll", async (req: Request, res: Response) => {
   const fileId = req.query.fileId;
