dd02ce2e04009fdba1a834551489660b60f14de7
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-08-02 15:12:20 +0800

    refactor: add the function check

--- src/storage/feat/version/doc_history.ts
+++ src/storage/feat/version/doc_history.ts
@@ -3,7 +3,6 @@ import { getThrottledFn } from "@/common/app/throttle_util.js";
 import logger from "@/common/log4js_config.js";
 import { SyncFileAttr } from "@/model/texhub/sync_file_attr";
 import { PgHisotoryPersistance } from "@/storage/adapter/postgresql/pg_history_persistance.js";
-import { throttledHistoryFn } from "@/storage/appfile.js";
 import * as Y from "rdyjs";
 
 export const pgHistoryDb: PgHisotoryPersistance = new PgHisotoryPersistance();
@@ -15,7 +14,9 @@ export async function handleHistoryDoc(
   try {
     const docId = syncFileAttr.docIntId!;
     const throttledSave =  getThrottledFn(docId)(syncFileAttr, ydoc);
-    await throttledSave(syncFileAttr, ydoc);
+    if (typeof throttledSave === 'function') {
+      await throttledSave(syncFileAttr, ydoc);
+    }
   } catch (error: any) {
     logger.error("save history doc error", error);
   }
