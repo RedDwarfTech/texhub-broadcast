ec2c84b0b97740b61aa0b928ecac14e509b73bad
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-09-14 19:12:33 +0800

    refactor: remove leveldb

--- src/common/async/sync_to_pg.ts
+++ src/common/async/sync_to_pg.ts
@@ -1,10 +1,8 @@
 import { parentPort } from "worker_threads";
-import { iterateAllLeveldbKeys } from "../migration/leveldb_to_postgresql.js";
 import logger from "../log4js_config.js";
 
 parentPort!.postMessage(doSyncSeperateWay());
 
 function doSyncSeperateWay() {
   logger.info("start sync from leveldb to postgresql...");
-  iterateAllLeveldbKeys();
 }
