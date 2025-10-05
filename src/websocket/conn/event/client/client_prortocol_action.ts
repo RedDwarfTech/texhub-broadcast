d93822cdb8cb185c03384151926a15ac86770e2f
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-10-05 13:28:49 +0800

    refactor: tweak the send sync step

--- src/websocket/conn/event/client/client_prortocol_action.ts
+++ src/websocket/conn/event/client/client_prortocol_action.ts
@@ -3,15 +3,27 @@ import { SyncMessageType } from "@/model/texhub/sync_msg_type.js";
 import * as encoding from "rdlib0/encoding";
 // @ts-ignore
 import * as syncProtocol from "rdy-protocols/sync";
-import { SocketIOClientProvider } from "../../socket_io_client_provider.js";
 import { Socket } from "socket.io-client";
+import { v4 as uuidv4 } from "uuid";
+import { SyncMessageContext } from "@/model/texhub/sync_msg_context.js";
+// @ts-ignore
+import * as Y from "rdyjs";
 
 export const clientSendSyncStep1 = (
-  provider: SocketIOClientProvider,
-  socketio: Socket
+  docName: string,
+  socketio: Socket,
+  doc: Y.Doc
 ) => {
   const encoder = encoding.createEncoder();
   encoding.writeVarUint(encoder, SyncMessageType.SubDocMessageSync);
-  syncProtocol.writeSyncStep1(encoder, provider.doc);
+  const uniqueValue = uuidv4();
+  let msg: SyncMessageContext = {
+    doc_name: docName,
+    src: "providerdocs",
+    trace_id: uniqueValue,
+  };
+  let msgStr = JSON.stringify(msg);
+  encoding.writeVarString(encoder, msgStr);
+  syncProtocol.writeSyncStep1(encoder, doc);
   socketio.send(encoding.toUint8Array(encoder));
 };
--- src/websocket/conn/event/client/subdoc_connect_handler.ts
+++ src/websocket/conn/event/client/subdoc_connect_handler.ts
@@ -1,11 +1,4 @@
-import { SyncMessageType } from "@/model/texhub/sync_msg_type.js";
 import { SocketIOClientProvider } from "../../socket_io_client_provider.js";
-// @ts-ignore
-import * as encoding from "rdlib0/encoding";
-import { v4 as uuidv4 } from "uuid";
-import { SyncMessageContext } from "@/model/texhub/sync_msg_context.js";
-// @ts-ignore
-import * as syncProtocol from "rdy-protocols/sync";
 import { Socket } from "socket.io-client";
 import { clientSendSyncStep1 } from "./client_prortocol_action.js";
 
@@ -21,21 +14,10 @@ export const handleSubdocConnect = (
       // so the server will response with step 2 and the sync events will be fired
       // then we can initialize the first texhub document
       // without the sync step 1, the editor will be empty for the first time
-      clientSendSyncStep1(provider, socketio);
+      clientSendSyncStep1(k, socketio, doc);
     } else {
       // this is a sub document, we send the sync step 1 with the sub doc message style
-      const encoder = encoding.createEncoder();
-      encoding.writeVarUint(encoder, SyncMessageType.SubDocMessageSync);
-      const uniqueValue = uuidv4();
-      let msg: SyncMessageContext = {
-        doc_name: k,
-        src: "providerdocs",
-        trace_id: uniqueValue,
-      };
-      let msgStr = JSON.stringify(msg);
-      encoding.writeVarString(encoder, msgStr);
-      syncProtocol.writeSyncStep1(encoder, doc);
-      socketio.send(encoding.toUint8Array(encoder));
+      clientSendSyncStep1(k, socketio, doc);
     }
   }
 };
